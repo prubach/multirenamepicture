1651ab21b04a3448352cc39f2b1bff737bd49ff5
Paweł Rubach <pawel.rubach@sorcersoft.com>
2016-11-01 11:00:24 +0100

    Added saving last processed directory

--- multirenamepicture-core/src/main/java/pl/waw/rubach/RenameImages.java
+++ multirenamepicture-core/src/main/java/pl/waw/rubach/RenameImages.java
@@ -20,8 +20,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Properties;
 
 /**
  * Created by pol on 08.10.16.
@@ -37,6 +37,8 @@ public class RenameImages {
     private JPanel panel1;
     private JLabel formatLabel;
     private JCheckBox cameraName;
+
+    private File propFile = new File(System.getProperty("user.home") + File.separator + ".renimages.properties");
 
     private File dirFile;
 
@@ -54,12 +56,13 @@ public class RenameImages {
         chooseDir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                final JFileChooser fc = new JFileChooser();
+                final JFileChooser fc = new JFileChooser(readLastDirectory());
                 fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
                 int returnVal = fc.showOpenDialog(directory);
                 if (returnVal == JFileChooser.APPROVE_OPTION) {
                     dirFile = fc.getSelectedFile();
                     directory.setText(dirFile.getAbsolutePath());
+                    saveLastDirectory(dirFile);
                 }
             }
         });
@@ -79,6 +82,55 @@ public class RenameImages {
         });
 
 
+    }
+
+    public File readLastDirectory() {
+        if (propFile.exists() && propFile.canRead()) {
+            Properties prop = new Properties();
+            InputStream input = null;
+            try {
+                input = new FileInputStream(propFile);
+                // load a properties file
+                prop.load(input);
+                // get the property value and print it out
+                String lastDirS = prop.getProperty("lastdir");
+                if ((lastDirS!=null) && (new File(lastDirS).exists() && new File(lastDirS).isDirectory()))
+                    return new File(lastDirS);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } finally {
+                if (input != null) {
+                    try {
+                        input.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return new File(System.getProperty("user.home"));
+    }
+
+    public void saveLastDirectory(File lastDir) {
+        Properties prop = new Properties();
+        OutputStream output = null;
+
+        try {
+            output = new FileOutputStream(propFile);
+            // set the properties value
+            prop.setProperty("lastdir", lastDir.getAbsolutePath());
+            prop.store(output, null);
+        } catch (IOException io) {
+            io.printStackTrace();
+        } finally {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public JPanel getPanel1() {
